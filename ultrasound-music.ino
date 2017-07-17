190ea952862394a08cd911024168bbbc18390104
Igor Abreu <igor@grama.cc>
2017-07-17 14:49:28 -0300

    indent code

--- ultrasound-music.ino
+++ ultrasound-music.ino
@@ -11,40 +11,38 @@ boolean ultraOneIn = true;
 
 
 void setup(){
-Serial.begin(9600);
-pinMode(trigPin, OUTPUT);
-pinMode(echoPin, INPUT);
-tmrpcm.speakerPin = 46;
-tmrpcm.setVolume(6);
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  tmrpcm.speakerPin = 46;
+  tmrpcm.setVolume(6);
 
-if (!SD.begin(SD_ChipSelectPin)) {
-Serial.println("SD fail");
-return;
-}
-
+  if (!SD.begin(SD_ChipSelectPin)) {
+    Serial.println("SD fail");
+    return;
+  }
 }
 
 void loop() {
-long duration, distance;
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2); 
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2); 
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 
-duration = pulseIn(echoPin, HIGH);
-distance = (duration/2) / 29.1;
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
 
-Serial.print(distance);
-Serial.println(" cm");
+  Serial.print(distance);
+  Serial.println(" cm");
 
-if(distance <6 && ultraOneIn){
+  if(distance <6 && ultraOneIn){
     tmrpcm.play("1.wav");
     ultraOneIn = false; 
-}else if(distance >6){
+  }else if(distance >6){
     ultraOneIn = true;
-}
+  }
 
-delay(100);
-
+  delay(100);
 }
